4cd1800cdab7cc89a662698bcdf1c855d83d1652
franco94 <francostavares2003@gmail.com>
2019-10-01 06:17:39 -0300

    Correcoes nos layouts e activitys.

--- app/src/main/java/br/eti/tavares/trashpick/ObjetivosActivity.java
+++ app/src/main/java/br/eti/tavares/trashpick/ObjetivosActivity.java
@@ -2,7 +2,9 @@ package br.eti.tavares.trashpick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ObjetivosActivity extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class ObjetivosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_objetivos);
     }
+
+    public void OnClickMaps(View v) {
+        Intent iMaps = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(iMaps);
+    }
+    public void OnClickRanking(View v) {
+        Intent iRanking = new Intent(getApplicationContext(), RankingActivity.class);
+        startActivity(iRanking);
+    }
+    public void OnClickPerfil(View v) {
+        Intent iPerfil = new Intent(getApplicationContext(), PerfilActivity.class);
+        startActivity(iPerfil);
+    }
 }
+
+
