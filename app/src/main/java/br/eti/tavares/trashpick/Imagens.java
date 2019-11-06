f10dc6fbb35fb511494184cab7f09d2641c41558
franco94 <francostavares2003@gmail.com>
2019-11-06 06:34:18 -0300

    Classe Imagens com novos icones adicionados

--- app/src/main/java/br/eti/tavares/trashpick/Imagens.java
+++ app/src/main/java/br/eti/tavares/trashpick/Imagens.java
@@ -22,8 +22,65 @@ public class Imagens {
                 return R.drawable.ic_garrafavidro_round;
             case "ic_embalagempet_round":
                 return R.drawable.ic_embalagempet_round;
+            case "ic_aluminio_round":
+                return R.drawable.ic_aluminio_round;
+            case "ic_borracha_round":
+                return R.drawable.ic_borracha_round;
+            case "ic_ceramica_round":
+                return R.drawable.ic_ceramica_round;
+            case "ic_chiclete_round":
+                return R.drawable.ic_chiclete_round;
+            case "ic_cigarro_round":
+                return R.drawable.ic_cigarro_round;
+            case "ic_couro_round":
+                return R.drawable.ic_couro_round;
+            case "ic_embalagempapel_round":
+                return R.drawable.ic_embalagempapel_round;
+            case "ic_entulho_round":
+                return R.drawable.ic_entulho_round;
+            case "ic_fralda_round":
+                return R.drawable.ic_fralda_round;
+            case "ic_garrafaplastica_round":
+                return R.drawable.ic_garrafaplastica_round;
+            case "ic_guardanapo_round":
+                return R.drawable.ic_guardanapo_round;
+            case "ic_lataaluminio_round":
+                return R.drawable.ic_lataaluminio_round;
+            case "ic_loucas_round":
+                return R.drawable.ic_loucas_round;
+            case "ic_madeirapintada_round":
+                return R.drawable.ic_madeirapintada_round;
+            case "ic_meiasla_round":
+                return R.drawable.ic_meiasla_round;
+            case "ic_metal_round":
+                return R.drawable.ic_metal_round;
+            case "ic_nylon_round":
+                return R.drawable.ic_nylon_round;
+            case "ic_oleocontaminado_round":
+                return R.drawable.ic_oleocontaminado_round;
+            case "ic_palitofosforo_round":
+                return R.drawable.ic_palitofosforo_round;
+            case "ic_pano_round":
+                return R.drawable.ic_pano_round;
+            case "ic_papelplastificado_round":
+                return R.drawable.ic_papelplastificado_round;
+            case "ic_pilhabateria_round":
+                return R.drawable.ic_pilhabateria_round;
+            case "ic_plastico_round":
+                return R.drawable.ic_plastico_round;
+            case "ic_pneu_round":
+                return R.drawable.ic_pneu_round;
+            case "ic_sacoplastico_round":
+                return R.drawable.ic_sacoplastico_round;
+            case "ic_tampinha_round":
+                return R.drawable.ic_tampinha_round;
             case "ic_luvasborracha_round":
                 return R.drawable.ic_luvasborracha_round;
+            case "ic_cascas_frutas_round":
+                return R.drawable.ic_cascas_frutas_round;
+            case "ic_copoplastico_round":
+                return R.drawable.ic_copoplastico_round;
+
             default:
                 return R.drawable.logotp;
         }
